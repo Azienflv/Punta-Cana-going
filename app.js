35827ba8e1e511cd7ae6a3e51fb980f46a3f3366
Azienflv <121687950+Azienflv@users.noreply.github.com>
2026-03-19 19:25:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,27 @@ function showSection(id) {
   document.getElementById(id).classList.add("active");
 }
 
-function guardarReserva() {
+async function guardarReserva() {
+
   const reserva = {
     cliente: cliente.value,
     hotel: hotel.value,
     tour: tour.value,
     adultos: adultos.value,
-    ninos: ninos.value
+    ninos: ninos.value,
+    fecha: new Date()
   };
+
+  try {
+    await db.collection("reservas").add(reserva);
+    alert("Guardado en la nube ☁️");
+
+    mostrarReservas();
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
   reservas.push(reserva);
   localStorage.setItem("reservas", JSON.stringify(reservas));
@@ -23,10 +36,15 @@ function guardarReserva() {
   alert("Guardado ✔");
 }
 
-function mostrarReservas() {
+async function mostrarReservas() {
+
   lista.innerHTML = "";
 
-  reservas.forEach(r => {
+  const querySnapshot = await db.collection("reservas").get();
+
+  querySnapshot.forEach((doc) => {
+    const r = doc.data();
+
     lista.innerHTML += `
       <div class="card">
         <b>${r.cliente}</b><br>
