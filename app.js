9daf2deb2bbdf06442feefc9006362061bc3ab02
Azienflv <121687950+Azienflv@users.noreply.github.com>
2026-03-21 19:04:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const sidebar = document.getElementById("sidebar");
+const main = document.getElementById("main");
+const toggleBtn = document.getElementById("toggleBtn");
 
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  main.classList.toggle("shift");
+});
+
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => {
+  item.addEventListener("click", () => {
+    menuItems.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
